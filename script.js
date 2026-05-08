187d68840f0bd3bc7b769ae174dc20e2d27fcda3
kimura <kimura.com>
2026-05-08 14:53:57 +0900

    h

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const omikujiImg = document.getElementById("araya");
 const result = document.getElementById("ryousyuu");
 const advice = document.getElementById("advice");
 
-omikujiImg.innerHTML ="<img src='happy.png'>"
+komikujiImg.innerHTML ="<img src='happy.png'>"
 
 button .addEventListener("click",function(){
     const randomNumber = Math.floor(Math.random() * fortunes.length);
